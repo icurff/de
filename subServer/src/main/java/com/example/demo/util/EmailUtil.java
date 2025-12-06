1f2bed870c0e23fd24a9bdd4f6e7b369eca1bd6e
Hai Nguyen <icurff@gmail.com>
2025-12-07 00:10:15 +0700

    ok

--- subServer/src/main/java/com/example/demo/util/EmailUtil.java
+++ subServer/src/main/java/com/example/demo/util/EmailUtil.java
@@ -1,27 +1,27 @@
-// package com.example.demo.util;
+package com.example.demo.util;
 
-// import org.springframework.mail.SimpleMailMessage;
-// import org.springframework.mail.javamail.JavaMailSender;
-// import org.springframework.scheduling.annotation.Async;
-// import org.springframework.stereotype.Service;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
 
-// @Service
-// public class EmailUtil {
+@Service
+public class EmailUtil {
 
-//     private final JavaMailSender emailSender;
+    private final JavaMailSender emailSender;
 
-//     public EmailUtil(JavaMailSender emailSender) {
-//         this.emailSender = emailSender;
-//     }
-//     @Async
-//     public void sendEmail(
-//             String to, String subject, String text) {
-//         SimpleMailMessage message = new SimpleMailMessage();
-//         message.setFrom("icurff@gmail.com");
-//         message.setTo(to);
-//         message.setSubject(subject);
-//         message.setText(text);
-//         emailSender.send(message);
+    public EmailUtil(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+    @Async
+    public void sendEmail(
+            String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("icurff@gmail.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
 
-//     }
-// }
+    }
+}
