733acab84cfab7e1f776dd9395ac8ddb8645dd41
Hai Nguyen <icurff@gmail.com>
2025-12-22 12:18:15 +0700

    update

--- centralServer/src/main/java/com/example/demo/repository/LivestreamRepository.java
+++ centralServer/src/main/java/com/example/demo/repository/LivestreamRepository.java
@@ -3,7 +3,6 @@ package com.example.demo.repository;
 import com.example.demo.model.Livestream;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface LivestreamRepository extends MongoRepository<Livestream, String> {
