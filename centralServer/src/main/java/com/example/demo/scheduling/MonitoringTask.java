e29b97d9fa807b4e1ffb0cf57cb357b01ef899dc
Hai Nguyen <icurff@gmail.com>
2025-12-25 05:50:50 +0700

    ok

--- centralServer/src/main/java/com/example/demo/scheduling/MonitoringTask.java
+++ centralServer/src/main/java/com/example/demo/scheduling/MonitoringTask.java
@@ -21,7 +21,7 @@ public class MonitoringTask {
     private ServerService serverService;
 
     // Chạy sau 5s và lặp lại mỗi 60s
-    @Scheduled(fixedDelay = 60 * 1000, initialDelay = 5 * 1000)
+    @Scheduled(fixedDelay = 10 * 1000, initialDelay = 5 * 1000)
     public void monitorTask() {
         List<Server> servers = serverService.getAllServers();
 
